9f6110fb9376d5cf800c74ea75ca38b693ddd6fb
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-03 16:36:23 +0200

    Add exercise 2_obwod_kola (lab02)

---
+++ 2_obwod_kola/2_obwod_kola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_obwod_kola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p, r, o;
+
+            p = (double)inputValue();
+            r = (double)Math.Sqrt( p/ Math.PI);
+            o = (double)(2 * Math.PI * r);
+            Console.WriteLine(o);
+        }
+
+        private static double inputValue()
+        {
+            double result;
+
+            while (!double.TryParse(Console.ReadLine(), out result)) ;
+
+            return result;
+        }
+    }
+}
