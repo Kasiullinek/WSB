1da67acd666e3c35a9fa944ce419b8edaed52e25
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-03 19:20:29 +0200

    Add exercise 7_obreb_kola

---
+++ 7_obreb_kola/7_obreb_kola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_obreb_kola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, r, l, p;
+            Console.Write("Podaj współrzędną x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Podaj współrzędną y: ");
+            y = double.Parse(Console.ReadLine());
+            Console.Write("Podaj promień r koła: ");
+            r = double.Parse(Console.ReadLine());
+
+            l = Math.Pow(x, 2) + Math.Pow(y, 2);
+            p = Math.Pow(r, 2);
+
+            if (l == p)
+            {
+                Console.WriteLine("Punkt (" + x +","+ y + ") leży w obrębie koła o środku O(0,0) i promieniu " + r );
+            }
+            else
+            {
+                Console.WriteLine("Punkt (" + x + "," + y + ") nie leży w obrębie koła o środku O(0,0) i promieniu " + r);
+            }
+            Console.ReadKey();
+        }
+    }
+}
