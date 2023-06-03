cf41d9664d470d18962b78e1a2c0166482bf1185
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-03 16:12:43 +0200

    Add exercise 1_srednia_arytmetyczna (lab02)

---
+++ 1_srednia_arytmetyczna/1_srednia_arytmetyczna/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_srednia_arytmetyczna
+{
+    class Program  {
+        static void Main(string[] args)  {
+            double a, b, c = 0;
+
+            a = inputValue();
+            b = inputValue();
+            c = inputValue();
+
+            double sredniaArytmetyczna;
+
+            sredniaArytmetyczna = (a + b + c) / 3;
+            Console.WriteLine(sredniaArytmetyczna);
+        }
+
+        private static double inputValue()    {
+            double result;
+
+            while (!double.TryParse(Console.ReadLine(), out result)) ;
+
+            return result;
+        }
+    }
+}
