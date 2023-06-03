484a79c577abc31bee0be1a0cbab5f7aaa8e360a
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-03 18:58:28 +0200

    Add exercise 4_ulamki and 5_wieksza_liczba

---
+++ 4_ulamki/4_ulamki/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ulamki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, x, y;
+          
+            Console.Write("a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("d: ");
+            d = int.Parse(Console.ReadLine());
+
+            if (b == 0 || d == 0)
+            {
+                Console.WriteLine("b i d nie może być równe 0!");
+            }else
+            {
+                y = b * d;
+                x = (a * d) + (c * b);
+                Console.WriteLine(x + "/"+ y + " = " + a + "/" + b + " + " + c + "/" + d);
+            }
+
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ 5_wieksza_liczba/5_wieksza_liczba/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_wieksza_liczba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2;
+            Console.Write("Podaj pierwszą liczbę: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj drugą liczbę: ");
+            n2 = double.Parse(Console.ReadLine());
+            if (n1 > n2)
+            {
+                Console.WriteLine("Większa jest: " + n1);
+            } else if (n2 > n1)
+            {
+                Console.WriteLine("Większa jest: " + n2);
+            } else
+            {
+                Console.WriteLine("Obie liczby są równe");
+            }
+            Console.ReadKey();
+        }
+    }
+}
