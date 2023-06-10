09fd56d3c68361334155907b3f59557da0b7d0e3
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-10 08:57:26 +0200

    Add exercise 1_potegowanie

---
+++ lab03/1_potegowanie/1_potegowanie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_potegowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, x, t = 1;
+
+            Console.Write("Wpisz podstawe: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Wpisz wykladnik: ");
+            x = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < x; i++)
+            {
+                t *= a;
+            }
+
+            Console.WriteLine(a + " do potegi " + x + " = " + t);
+            Console.ReadKey();
+        }
+    }
+}
