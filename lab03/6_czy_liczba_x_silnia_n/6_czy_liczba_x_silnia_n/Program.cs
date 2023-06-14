fc0c33543df1368395c4004b97b33838ac9cc145
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-15 01:13:45 +0200

    Add new project 6_czy_liczba_x_silnia_n

---
+++ lab03/6_czy_liczba_x_silnia_n/6_czy_liczba_x_silnia_n/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_czy_liczba_x_silnia_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                double n = 0, x = 0;
+
+                Console.Write("Podaj liczbę n: ");
+                n = double.Parse(Console.ReadLine());
+                Console.Write("Podaj liczbę x: ");
+                x = double.Parse(Console.ReadLine());
+
+                double nS = Silnia(n);
+
+                if (x == nS)
+                {
+                    Console.WriteLine("Wprowadzona liczba x jest silnią liczby n = " + n);
+                }
+                else
+                {
+                    Console.WriteLine("Wprowadzona liczba x nie jest silnią liczby n");
+                }
+
+                Console.WriteLine("\nNaciśniej Escape aby zakończyć działanie programu albo dowlony klawisz aby kontynuować... \n");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            
+        }
+        static double Silnia(double s)
+        {
+            double wynik = s;
+
+            if (s > 1)
+            {
+                wynik = s * Silnia(s - 1);
+            }
+            else
+            {
+                wynik = 1;
+            }
+
+            return wynik;
+        }
+    }
+}
