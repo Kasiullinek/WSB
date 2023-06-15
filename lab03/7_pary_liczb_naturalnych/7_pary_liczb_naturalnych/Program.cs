2c50a2f246a5952784cab3ca7b538d4c10fed222
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-15 23:33:23 +0200

    Add new project: 7_pary_liczb_naturalnych

---
+++ lab03/7_pary_liczb_naturalnych/7_pary_liczb_naturalnych/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_pary_liczb_naturalnych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int n = 0;
+
+                Console.Write("Podaj liczbę n: ");
+                n = int.Parse(Console.ReadLine());
+
+                Console.Write("Wszystkie pary liczb naturalnych, których suma jest = " + n + " są następujące: ");
+                for (int i = 0; i <= n; i++)
+                {
+                    Console.Write(" " + i + " i " + n + ", ");
+                    n--;
+                }
+
+                Console.WriteLine("\n\nNaciśnij Escape aby zakończyć działanie programu albo dowolony klawisz aby kontynuować... \n");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
