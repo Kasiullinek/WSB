358b282c3bbfa999172eb4a0c0eba9be896370eb
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-17 17:10:28 +0200

    5_sortowanie_przez_wybieranie

---
+++ lab04/5_sortowanie_przez_wybieranie/5_sortowanie_przez_wybieranie/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_sortowanie_przez_wybieranie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rozmiar = 10;
+
+            int[] tablica = new int[rozmiar];
+            Random random = new Random();
+
+            Console.Write("Nieposortowana tablica: ");
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                tablica[i] = random.Next(-10, 11);
+                Console.Write(tablica[i] + " ");
+            }
+
+            for (int i = 0; i < rozmiar - 1; i ++)
+            {
+                int j = 0;
+                int minIndex = i;
+
+                for(j = i + 1; j < rozmiar; j++)
+                {
+                    if(tablica[j] < tablica[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = tablica[i];
+                tablica[i] = tablica[minIndex];
+                tablica[minIndex] = temp;
+
+            }
+
+            Console.Write("\n\nPosortowana tablica: ");
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                Console.Write(tablica[i] + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ lab04/5_sortowanie_przez_wybieranie/5_sortowanie_przez_wybieranie/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("5_sortowanie_przez_wybieranie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("5_sortowanie_przez_wybieranie")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("9d9e6b6b-1f4e-4dba-a8d0-f444c7aa3005")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
