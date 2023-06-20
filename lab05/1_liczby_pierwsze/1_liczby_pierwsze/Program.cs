b8c7c89ab727f1966a40595cfa448c643cdb3ee3
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-20 22:10:39 +0200

    Add new project: 1_liczby_pierwsze

---
+++ lab05/1_liczby_pierwsze/1_liczby_pierwsze/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_liczby_pierwsze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, licznik = 0, liczba = 2;
+
+            Console.WriteLine("Ile liczb pierwszych wyświetlić?");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("\nLiczby pierwsze: \n");
+
+            while (licznik < n) {
+
+                bool t = true;
+
+                for (int i = 2; i < liczba; i++)
+                {
+                    if(liczba % i == 0)
+                    {
+                        t = false;
+                        break;
+                    }
+                }
+
+                if(t)
+                {
+                    Console.WriteLine(liczba);
+                    licznik++;
+                }
+
+                liczba++;
+            }
+
+            Console.Read();
+        }
+    }
+}
