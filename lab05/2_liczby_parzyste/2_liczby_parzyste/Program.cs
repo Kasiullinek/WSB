57fb2f456f788af328e234afd46b4f5d96f7bde6
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-21 21:48:24 +0200

    Add new project: 2_liczby_parzyste

---
+++ lab05/2_liczby_parzyste/2_liczby_parzyste/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_liczby_parzyste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Ile liczb parzystych wypisać?");
+            n = int.Parse(Console.ReadLine());
+
+            PierwszaWersja(n);
+            //DrugaWersja(n);
+
+            Console.Read();
+        }
+        
+        static void PierwszaWersja(int n)
+        {
+            Random random = new Random();
+
+            int licznik = 0, liczba = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                do
+                {
+                    liczba = random.Next(-8, 9);
+                    licznik++;
+
+                } while (liczba % 2 != 0);
+
+                Console.Write(liczba);
+
+                if (i < n- 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine("\nLiczba wykonanych losowań: " + licznik);
+        }
+
+        static void DrugaWersja(int n)
+        {
+            Random random = new Random();
+
+            int licznik = 0, liczba = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                liczba = random.Next(-4, 5) * 2;
+                licznik++;
+
+                Console.Write(liczba);
+
+                if (i < n - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine("\nLiczba wykonanych losowań: " + licznik);
+        }
+    }
+}
---
+++ lab05/2_liczby_parzyste/2_liczby_parzyste/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("2_liczby_parzyste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_liczby_parzyste")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("45057f1e-7c81-433f-a397-ca91dec040b4")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
