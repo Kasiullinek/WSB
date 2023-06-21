3d35eea153fb5a7d44de7a4d753a339d9dbe130a
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-21 23:11:55 +0200

    Add new project: 3_trojkat

---
+++ lab05/3_trojkat/3_trojkat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_trojkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jak wysoki trójkąt narysować?");
+            int h = int.Parse(Console.ReadLine());
+
+            int licznik = h;
+
+            for (int i = 1; i <= h; i++)
+            {
+                for (int j = h-1; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= 2*i-1; k++)
+                {
+                    Console.Write("+");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.Read();
+        }
+    }
+}
