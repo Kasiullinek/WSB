87dcd3d1e2f1f7bff47c0be7ff6a9a9c2529304d
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-24 23:00:25 +0200

    Add new project: 2_dlugosc_odcinka

---
+++ lab07/2_dlugosc_odcinka/2_dlugosc_odcinka/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_dlugosc_odcinka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pX = 0, pY = 0, kX = 0, kY = 0;
+
+            Console.Write("Podaj współrzędną X początkowego punktu: ");
+            pX = double.Parse(Console.ReadLine());
+            Console.Write("Podaj współrzędną Y początkowego punktu: ");
+            pY = double.Parse(Console.ReadLine());
+            Console.Write("Podaj współrzędną X punktu końca: ");
+            kX = double.Parse(Console.ReadLine());
+            Console.Write("Podaj współrzędną Y punktu końca: ");
+            kY = double.Parse(Console.ReadLine());
+
+            double length = CalculateLength(pX, pY, kX, kY);
+
+            Console.WriteLine("\nDługość odcinka od punktu A({0},{1}) do punktu B({2},{3}) wynosi: {4}", pX, pX, kX, kY, length);
+
+            Console.Read();
+        }
+
+        static double CalculateLength(double pX, double pY, double kX, double kY)
+        {
+            double length = Math.Sqrt(Math.Pow(kX - pX, 2) + Math.Pow(kY - pY, 2));
+            return length;
+        }
+    }
+}
---
+++ lab07/2_dlugosc_odcinka/2_dlugosc_odcinka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("2_dlugosc_odcinka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_dlugosc_odcinka")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("51f09ff7-7468-4d84-80be-92443e7ec54c")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
