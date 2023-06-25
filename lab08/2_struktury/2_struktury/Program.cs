943e18af681373e5d6f0604ca892af9499a7b66c
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-25 18:03:29 +0200

    Add new project 2_struktury

---
+++ lab08/2_struktury/2_struktury/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_struktury
+{
+    class Program
+    {
+        public struct Student
+        {
+            public string Imie;
+            public string Nazwisko;
+            public int NumerAlbumu;
+            public string Kierunek;
+            public string Email;
+        }
+
+        public struct Przedmiot
+        {
+            public string Nazwa;
+            public string Kod;
+            public string Opis;
+            public int ECTS;
+            public string Prowadzacy;
+        }
+
+        public struct NauczycielAkademicki
+        {
+            public string Imie;
+            public string Nazwisko;
+            public string TytulNaukowy;
+            public string Email;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
