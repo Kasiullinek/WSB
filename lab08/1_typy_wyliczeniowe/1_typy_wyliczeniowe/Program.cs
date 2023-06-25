6df0d3615b45a1ee0914227efb4cffb4151e46a4
katarzynabator <bator.katarzyna01@gmail.com>
2023-06-25 17:27:09 +0200

    Add new project: 1_typy_wyliczeniowe

---
+++ lab08/1_typy_wyliczeniowe/1_typy_wyliczeniowe/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_typy_wyliczeniowe
+{
+    class Program
+    {
+        public enum DniTygodnia
+        {
+            Poniedziałek,
+            Wtorek,
+            Środa,
+            Czwartek,
+            Piątek,
+            Sobota,
+            Niedziela,
+        }
+
+        public enum EtapyPrania
+        {
+            Pranie,
+            Płukanie,
+            Wirowanie,
+        }
+
+        public enum Posiłki
+        {
+            Śniadanie,
+            DrugieŚniadanie,
+            Obiad,
+            Podwieczorek,
+            Kolacja,
+        }
+
+        public enum BierkiSzachowe
+        {
+            Król,
+            Dama,
+            Wieża,
+            Goniec,
+            Skoczek,
+            Pion,
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
